a6e2bb2e0e6c3438553d12ed2f2337ba2df0881c
dece1225 <dece1225@naver.com>
2025-08-07 21:00:04 +0900

    250807 20:59 김태현 지도 페이지 작업중

--- webapp/assets/js/map/map.js
+++ webapp/assets/js/map/map.js
@@ -1,7 +1,10 @@
 const mapImg = document.querySelector(".map_img");
 const sidebar = document.querySelector(".sidebar");
-
+const modalBox = document.querySelector(".modalbox");
+const closeBtn = document.querySelector(".closebtn");
+const wishBtn = document.querySelector(".wishbtn");
 
 mapImg.addEventListener("click", function(){
   sidebar.style.display = 'flex';
 })
+
