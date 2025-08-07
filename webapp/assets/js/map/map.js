426ed70b7cce4a60b181e8764c535bf1450fcdea
dece1225 <dece1225@naver.com>
2025-08-07 21:24:15 +0900

    250807 21:23 김태현 지도 페이지 html, css, js 작업 끝

--- webapp/assets/js/map/map.js
+++ webapp/assets/js/map/map.js
@@ -3,8 +3,25 @@ const sidebar = document.querySelector(".sidebar");
 const modalBox = document.querySelector(".modalbox");
 const closeBtn = document.querySelector(".closebtn");
 const wishBtn = document.querySelector(".wishbtn");
+const modal_closebtn = document.querySelector(".modal_closebtn");
 
-mapImg.addEventListener("click", function(){
+//이미지 클릭 시 사이드바 띄우기
+mapImg.addEventListener("click", function () {
   sidebar.style.display = 'flex';
 })
 
+//찜 버튼 클릭 시 모달창 띄우기
+wishBtn.addEventListener("click", function () {
+  modalBox.style.display = 'flex';
+})
+
+//사이드바 닫기 클릭 시 사이드바 none
+closeBtn.addEventListener("click", function () {
+  sidebar.style.display = 'none';
+})
+
+//모달창 닫기 클릭 시 로그인페이지 이동(지금은 더미페이지 이동)
+modal_closebtn.addEventListener("click", function () {
+  modalBox.style.display = 'none';
+  location.href = './../../dummy.html';
+})
