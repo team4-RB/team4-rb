f8d8f28cf626d834708db80bffde4e3bc6c5f0c5
BuddleK <kangbudle01@gmail.com>
2025-08-09 00:38:51 +0900

    250809 00:38 강버들 음식점 진행중

--- webapp/assets/js/restaurant/restaurant.js
+++ webapp/assets/js/restaurant/restaurant.js
@@ -1,5 +1,8 @@
 const reButtons = document.getElementsByClassName("region_button");
 const reMarks = document.getElementsByClassName("region_mark");
+const reSmall = document.getElementsByClassName("region_small");
+const reSmallList = document.getElementById("region_small_list");
+const reCards = document.getElementsByClassName("restaurant_card");
 
 var regionNum = 0;
 var arr = [
@@ -11,11 +14,65 @@ var arr = [
   ['잠원동']
 ];
 
+var restaurants = [
+  ['춘리', '방배동', '9시~11시', '010-1234-5678'],
+  ['오삼불고기', '역삼동', '8시~1시', '010-1234-5678'],
+  ['토스트', '강남', '9시~18시', '010-1234-5678']
+];
+
+function showCards(){
+
+  for(let i = 0 ;i < 4;i++){
+    if(i < restaurants.length){
+      let str = "";
+      str += `<div class="restaurant_name">` + restaurants[i][0];
+      str += `</div><div class="restaurant_introduce"><div class="restaurant_info"><div>` + restaurants[i][1];
+      str += `</div><div>` + restaurants[i][2];
+      str += `</div><div>` + restaurants[i][3];
+      str += `</div></div><div class="restaurant_mark"><div>` + "별";
+      str += `</div><div>` + "찜하기";
+      str += `</div></div></div></div>`;
+      reCards[i].innerHTML = str;
+      reCards[i].style.opacity = 1;
+    }else{
+      reCards[i].innerHTML = "";
+      reCards[i].style.opacity = 0;
+    }
+    
+  }
+}
+
 function reClicked(num){
+  // 입력한 숫자
   console.log("숫자는 : " + num);
+
+  // 판 보이기
+  reSmall[0].style.display = "block";
+
+  // 해당 행의 문자열 배열 출력
   console.log(arr[num], typeof arr[num]);
+
+  // 리스트 초기화
+  reSmallList.innerHTML = "";
+
+  // 각 문자열 출력
+  for(ar of arr[num]){
+    console.log(ar, typeof ar);
+
+    // let str = "";
+    // str += `<li>`;
+    // str += ar;
+    // str += `</li>`;
+
+    const li = document.createElement("li");
+    li.innerText = ar;
+
+    reSmallList.appendChild(li);
+  }
   
-  
+
+
+
 
   var j = 0;
   for(i of reMarks){
@@ -26,14 +83,9 @@ function reClicked(num){
     }
     j++;
   }
+
+  showCards();
+
+
+
 }
-
-
-window.addEventListener('DOMContentLoaded', () => {
-  // 헤더 불러오기
-  fetch('/team04-RB_frontend/webapp/header.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('h').innerHTML = data;
-    });
-});
