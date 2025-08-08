dfc2056f4aa1fa3700c7cd19c59d48f415520cda
BuddleK <kangbudle01@gmail.com>
2025-08-08 18:38:37 +0900

    250808 18:38 강버들 음식점 리스트 진행 중

--- webapp/assets/js/restaurant/restaurant.js
+++ webapp/assets/js/restaurant/restaurant.js
@@ -1,10 +1,22 @@
 const reButtons = document.getElementsByClassName("region_button");
 const reMarks = document.getElementsByClassName("region_mark");
 
-
+var regionNum = 0;
+var arr = [
+  ['방배 1동','방배 2동','방배 3동','방배 4동','방배 본동'],
+  ['반포 1동','반포 2동','반포 3동','반포 4동','반포 본동'],
+  ['서초 1동','서초 2동','서초 3동','서초 4동'],
+  ['양재 1동','양재 2동'],
+  ['내곡동'],
+  ['잠원동']
+];
 
 function reClicked(num){
   console.log("숫자는 : " + num);
+  console.log(arr[num], typeof arr[num]);
+  
+  
+
   var j = 0;
   for(i of reMarks){
     if(j==num){
@@ -15,3 +27,13 @@ function reClicked(num){
     j++;
   }
 }
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  // 헤더 불러오기
+  fetch('/team04-RB_frontend/webapp/header.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('h').innerHTML = data;
+    });
+});
