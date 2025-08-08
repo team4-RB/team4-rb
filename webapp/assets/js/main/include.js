ba7b0159522dd0ff3ea93cb4f34ab91ab82b26a9
BuddleK <kangbudle01@gmail.com>
2025-08-08 09:37:35 +0900

    250808 09:37 강버들 main/include.js 경로 수정

--- webapp/assets/js/main/include.js
+++ webapp/assets/js/main/include.js
@@ -1,13 +1,13 @@
 window.addEventListener('DOMContentLoaded', () => {
   // 헤더 불러오기
-  fetch('/team04-rb/webapp/header.html')
+  fetch('/team04-RB_frontend/webapp/header.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('header').innerHTML = data;
     });
 
   // 푸터 불러오기
-  fetch('/team04-rb/webapp/footer.html')
+  fetch('/team04-RB_frontend/webapp/footer.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('footer').innerHTML = data;
