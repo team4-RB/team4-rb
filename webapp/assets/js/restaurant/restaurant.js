c13c9c4ac89bb762b67c84e90cb07f476128d9d5
BuddleK <kangbudle01@gmail.com>
2025-08-08 11:24:51 +0900

    250808 11:24 강버들 음식점 js 진행중

--- webapp/assets/js/restaurant/restaurant.js
+++ webapp/assets/js/restaurant/restaurant.js
@@ -1,35 +1,17 @@
-// const myButton = document.getElementById("myButton");
-// const myDiv = document.getElementById("myDiv");
-
-// myButton.addEventListener("click", function(){
-//   if(myDiv.style.display === "none"){
-//     myDiv.style.display = "block";
-//   }else{
-//     myDiv.style.display = "none";
-//   }
-// });
-
-
 const reButtons = document.getElementsByClassName("region_button");
 const reMarks = document.getElementsByClassName("region_mark");
 
-// for(item of reButtons){
-//   item.addEventListener("click", function(){
-//     for(i of reMarks){
-//       i.style.display="none";  
-//     }
-//   });
-// }
 
-for(item of reButtons){
-  item.addEventListener("click", function(){
-    console.log(reButtons.index(item));
-    for(i of reMarks){
-      i.style.display="none";  
+
+function reClicked(num){
+  console.log("숫자는 : " + num);
+  var j = 0;
+  for(i of reMarks){
+    if(j==num){
+      i.style.display="block";
+    }else{
+      i.style.display="none";
     }
-  });
-}
-
-for(item of reButtons){
-  console.log(reButtons.index(item));
+    j++;
+  }
 }
