b8a7e49ca6dcc2b86008dc5df5398b39add98cbb
HwangSeungU <pett1475321@gmail.com>
2025-08-12 00:34:49 +0900

    250812 1234 황승우 관리자 완

--- webapp/assets/js/admin/admin.js
+++ webapp/assets/js/admin/admin.js
@@ -5,15 +5,3 @@ window.addEventListener("DOMContentLoaded", () => {
       document.getElementById("sidebar").innerHTML = data;
     });
 });
-
-const id = document.getElementById("id");
-const pw = document.getElementById("pw");
-
-function loginSuccess() {
-  if (id.value === "admin" && pw.value === "admin") {
-    location.href = link;
-    alert("로그인 완료, 관리자 페이지 이동");
-  } else {
-    alert("로그인 실패!!");
-  }
-}
