1d753ef2be40b7c5899d7811488aaee4c77a3721
jeabin5792 <woqls0512@naver.com>
2025-08-12 23:20:54 +0900

    250812 23:20 이재빈 게시판 js중

---
+++ webapp/assets/js/post/postMain.js
@@ -0,0 +1,31 @@
+const postWrite = document.querySelector('.postWrite');
+const modal = document.querySelector('.modal');
+const postTitle = document.querySelector('.postTitle');
+
+// let user = "login";
+let user = "myPost";
+// let user = "notLogin";
+
+
+function postWrite() {
+  if (user == "myPost" || "login") {
+    window.location.href = "./postWrite.html";
+  } else {
+    modal.style.display = 'block';
+  }
+}
+function postTitle() {
+  if (user == "myPost") {
+    window.location.href = "./postDetaillogin.html";
+  } else {
+    window.location.href = "./postDetailNotlogin.html";
+  }
+}
+
+// postTitle.addEventListener('click', function () {
+//   if(user == myPost){
+//     window.location.href = "./postDetaillogin.html";
+//   }else{
+//     window.location.href = "./postDetailNotlogin.html";
+//   }
+// });
