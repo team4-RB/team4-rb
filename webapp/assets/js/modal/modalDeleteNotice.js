b3855d35e7282ad95741bb767fe09348b910ba23
HwangSeungU <pett1475321@gmail.com>
2025-08-12 14:10:47 +0900

    250812 1410 황승우 사이드바 수정

--- webapp/assets/js/modal/modalDeleteNotice.js
+++ webapp/assets/js/modal/modalDeleteNotice.js
@@ -1,16 +1,6 @@
-
-window.addEventListener("DOMContentLoaded", () => {
-  fetch("../../app/admin/sidebar.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("sidebar").innerHTML = data;
-    });
-});
-
-
 window.addEventListener('DOMContentLoaded', () => {
   // html 문서에 모달 불러오기
-  fetch('./../../../app/modal/modalDeleteNotice.html')
+  fetch('/team04-RB_frontend/webapp/app/modal/modalDeleteNotice.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('modalDeleteNotice').innerHTML = data;
@@ -22,10 +12,10 @@ const modalDeleteNotice = document.getElementById("modalDeleteNotice");
 
 // 모달이 나타나는 함수(버튼에 추가하여 사용)
 function modalDeleteNoticeShow() {
-  // event.preventDefault();
+  event.preventDefault();
   console.log("모달버튼클릭");
   modalDeleteNotice.style.display = "flex";
-  
+
   //엔터로 확인 버튼 누르기
   document.onkeydown = (event) => {
     if(event.key === "Enter"){
@@ -58,4 +48,3 @@ function modalDeleteNoticeCancel(){
   modalDeleteNoticeNone();
 }
 modalDeleteNoticeNone();
-
--- webapp/assets/js/modal/modalEditName.js
+++ webapp/assets/js/modal/modalEditName.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
   // html 문서에 모달 불러오기
-  fetch('./../../../app/modal/modalEditName.html')
+  fetch('/team04-RB_frontend/webapp/app/modal/modalEditName.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('modalEditName').innerHTML = data;
